27d28f3101814c9333df78a1f08b8e7239ae69cd
Sébastien Han <seb@redhat.com>
2015-11-25 15:49:50 +0100

    Initial commit
    Co-Authored-By: Deepak C Shetty <deepakcs@redhat.com>
    Change-Id: I124b54b5068fbcd46e278281da12ea27f0034d1f

---
+++ devstack/plugin.sh
@@ -0,0 +1,89 @@
+# ceph.sh - DevStack extras script to install Ceph
+
+if [[ "$1" == "source" ]]; then
+    # Initial source
+    source $TOP_DIR/lib/ceph
+elif [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+    echo_summary "Installing Ceph"
+    check_os_support_ceph
+    if [ "$REMOTE_CEPH" = "False" ]; then
+        install_ceph
+        echo_summary "Configuring Ceph"
+        configure_ceph
+        # NOTE (leseb): we do everything here
+        # because we need to have Ceph started before the main
+        # OpenStack components.
+        # Ceph OSD must start here otherwise we can't upload any images.
+        echo_summary "Initializing Ceph"
+        init_ceph
+        start_ceph
+    else
+        install_ceph_remote
+    fi
+elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+    if is_service_enabled glance; then
+        echo_summary "Configuring Glance for Ceph"
+        configure_ceph_glance
+    fi
+    if is_service_enabled nova; then
+        echo_summary "Configuring Nova for Ceph"
+        configure_ceph_nova
+    fi
+    if is_service_enabled cinder; then
+        echo_summary "Configuring Cinder for Ceph"
+        configure_ceph_cinder
+    fi
+    if is_service_enabled cinder || is_service_enabled nova; then
+        # NOTE (leseb): the part below is a requirement
+        # to attach Ceph block devices
+        echo_summary "Configuring libvirt secret"
+        import_libvirt_secret_ceph
+    fi
+
+    if [ "$REMOTE_CEPH" = "False" ]; then
+        if is_service_enabled glance; then
+            echo_summary "Configuring Glance for Ceph"
+            configure_ceph_embedded_glance
+        fi
+        if is_service_enabled nova; then
+            echo_summary "Configuring Nova for Ceph"
+            configure_ceph_embedded_nova
+        fi
+        if is_service_enabled cinder; then
+            echo_summary "Configuring Cinder for Ceph"
+            configure_ceph_embedded_cinder
+        fi
+        # FIXME: Fix this once radosgw service is running
+
+        #echo_summary "Configuring Rados Gateway with Keystone for Swift"
+        #configure_ceph_embedded_rgw
+    fi
+    if [ "$REMOTE_CEPH_RGW" = "True" ]; then
+        if is_service_enabled swift; then
+            die $LINENO \
+            "You can not activate both Swift and Ceph Rados Gateway, \
+            please disable Swift or set REMOTE_CEPH_RGW=False"
+        else
+            configure_ceph_remote_radosgw
+        fi
+    fi
+fi
+
+if [[ "$1" == "unstack" ]]; then
+    if [ "$REMOTE_CEPH" = "True" ]; then
+        cleanup_ceph_remote
+    else
+        cleanup_ceph_embedded
+        stop_ceph
+    fi
+    cleanup_ceph_general
+fi
+
+if [[ "$1" == "clean" ]]; then
+    if [ "$REMOTE_CEPH" = "True" ]; then
+        cleanup_ceph_remote
+    else
+        cleanup_ceph_embedded
+    fi
+    cleanup_ceph_general
+fi
